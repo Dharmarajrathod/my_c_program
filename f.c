4a334ce9d9c9bf13ca08fd90164069864123a42e
Dharmarajrathod <rathoddharmaraj2512@gmail.com>
2023-08-01 22:15:08 +0530

    sixth c program

---
+++ f.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("Enter a:- ");
+	scanf("%d",&a);
+	int b;
+	printf("\nEnter b:- ");
+	scanf("%d",&b);
+	int sum = a+b;
+	printf("The sum is %d",sum);
+	return 0;
+}

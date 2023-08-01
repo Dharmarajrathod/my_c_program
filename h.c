3213b3437fed3f57fcbb612d11c1008a6c423eb7
Dharmarajrathod <rathoddharmaraj2512@gmail.com>
2023-08-01 22:35:03 +0530

    eight c program

---
+++ h.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	float radius;
+	printf("Enter the radius of cirlce:- ");
+	scanf("%f",&radius);
+	float pi = 3.14;
+	printf("The area of circle is %f",pi * radius * radius);
+	return 0;
+}

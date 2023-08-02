fb3cb3cc173f2dba0792a6c15d9d2f8257fecbdc
Dharmarajrathod <rathoddharmaraj2512@gmail.com>
2023-08-02 20:58:29 +0530

    my c prog

---
+++ q.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter a number:- ");
+	scanf("%d",&a);
+	int b;
+	printf("enter b number:- ");
+	scanf("%d",&b);
+	int c;
+	printf("enter c number:- ");
+	scanf("%d",&c);
+	printf("%d", (a+b+c)/3 );
+}
---
+++ r.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("enter a number:- ");
+	scanf("%d",&a);
+	int b;
+	printf("enter b number:- ");
+	scanf("%d",&b);
+	printf("%d", a>b );
+}
